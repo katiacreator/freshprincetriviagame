b4db4be5f2dabc255eb1484e06d18999b43b2627
Katia Gibson <kcygibson@gmail.com>
2021-07-05 15:06:40 -0400

    completed template literal for home page, changed some of the variable names around

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
+console.log("we live!");
 //* Title Card Object */
-const titleCard = {
-  gameLogoImg: `<img src="placeholder.jpg" alt="game logo placeholder">`,
-  gameInstructions: `<h2 class="gameInstructions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vero mollitia, minima impedit nobis cumque veniam asperiores vitae nesciunt quam!</h2>`,
-  nextBtn = `<button class="continueBtn"><i class="fas fa-long-arrow-alt-right"></i>CLICK NEXT</button>`
-  //functions?
-}
-console.log(titleCard.gameLogoImg)
+const homeScreen = {
+  img: `<img src="placeholder.jpg" alt="game logo placeholder">`,
+  instructions: `<h2 class="gameInstructions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vero mollitia, minima impedit nobis cumque veniam asperiores vitae nesciunt quam!</h2>`,
+  continueBtn: `<button class="continueBtn"><i class="fas fa-long-arrow-alt-right"></i>CONTINUE</button>`,
+};
+
+const markup = `<!-- div for logo img -->
+<section id="logo-card" class="row-1 flipCard">
+${homeScreen.img}
+</section>
+<!-- div to instructions -->
+<section id="instructions-card">
+${homeScreen.instructions}
+<!-- this is a button to go to categories screen -->
+${homeScreen.continueBtn}
+</section>`;
+
+console.log('markup: ', markup);
+const content = document.getElementById("content");
+content.innerHTML = markup
+console.log(content)
