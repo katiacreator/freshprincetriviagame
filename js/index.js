f1768f2f79613a40bd1639019f75cc900e457820
Katia Gibson <kcygibson@gmail.com>
2021-07-06 07:56:06 -0400

    delete font webkit because i will not have time to get it working for this project, added fresh prince logo photo to start workingon card, updated img tag in the html markup

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 console.log("we live!");
 //* Title Card Object */
 const homeScreen = {
-  img: `<img src="placeholder.jpg" alt="game logo placeholder">`,
+  img: `<img src="/assets/freshprince/freshprince-logo.png" alt="game logo placeholder" class="img">`,
   instructions: `<h2 class="gameInstructions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vero mollitia, minima impedit nobis cumque veniam asperiores vitae nesciunt quam!</h2>`,
-  continueBtn: `<button class="continueBtn"><i class="fas fa-long-arrow-alt-right"></i>CONTINUE</button>`,
+  continueBtn: `<button id="continueBtn"><i class="fas fa-long-arrow-alt-right btn"> CONTINUE</i></button>`,
 };
 
 const markup = `<!-- div for logo img -->
 <section id="logo-card" class="row-1 flipCard">
 ${homeScreen.img}
+<p>TRIVIA GAME</p>
 </section>
 <!-- div to instructions -->
 <section id="instructions-card">
