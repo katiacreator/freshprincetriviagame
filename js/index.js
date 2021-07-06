b82149eff037c4f227d3a17e976aea03daf1700e
Katia Gibson <kcygibson@gmail.com>
2021-07-06 08:58:59 -0400

    completed initial styling of home page

--- js/index.js
+++ js/index.js
@@ -2,17 +2,14 @@ console.log("we live!");
 //* Title Card Object */
 const homeScreen = {
   img: `<img src="/assets/freshprince/freshprince-logo.png" alt="game logo placeholder" class="img">`,
-  instructions: `<h2 class="gameInstructions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vero mollitia, minima impedit nobis cumque veniam asperiores vitae nesciunt quam!</h2>`,
-  continueBtn: `<button id="continueBtn"><i class="fas fa-long-arrow-alt-right btn"> CONTINUE</i></button>`,
+  instructions: `<h3 class="instructions">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore vero mollitia, minima impedit nobis cumque veniam asperiores vitae nesciunt quam!</h3>`,
+  continueBtn: `<p><button id="continueBtn"><a href="categories.html"><i class="fas fa-long-arrow-alt-right btn">CONTINUE</i></a></button></p>`,
 };
 
 const markup = `<!-- div for logo img -->
-<section id="logo-card" class="row-1 flipCard">
+<section id="logo-card" class="logo-card">
 ${homeScreen.img}
-<p>TRIVIA GAME</p>
-</section>
-<!-- div to instructions -->
-<section id="instructions-card">
+<h1>TRIVIA GAME</h1>
 ${homeScreen.instructions}
 <!-- this is a button to go to categories screen -->
 ${homeScreen.continueBtn}
