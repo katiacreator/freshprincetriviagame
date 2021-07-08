c8bfa1589f53e1ef62fa765d5dceecdd7df836e2
Katia Gibson <kcygibson@gmail.com>
2021-07-08 12:11:16 -0400

    finishing css styling for categories page

--- js/category.js
+++ js/category.js
@@ -2,27 +2,22 @@ console.log("we live");
 //* Category Card Object *//
 const categoryArray = [
   {
-    icon: `<i class= "fas fa-signature"></i>`,
     category: `<p id="category">GUEST STARS</p>`,
     description: `<p id="description">Lorem1 ipsum dolor sit amet.</p>`,
   },
   {
-    icon: `<i class= "fas fa-signature"></i>`,
     category: `<p id="category">PLOTLINES</p>`,
     description: `<p id="description">Lorem1 ipsum dolor sit amet.</p>`,
   },
   {
-    icon: `<i class= "fas fa-signature"></i>`,
     category: `<p id="category">BEHIND THE CAMERA</p>`,
     description: `<p id="description">Lorem1 ipsum dolor sit amet.</p>`,
   },
   {
-    icon: `<i class= "fas fa-signature"></i>`,
     category: `<p id="category">CHARACTER NAMES</p>`,
     description: `<p id="description">Lorem1 ipsum dolor sit amet.</p>`,
   },
   {
-    icon: `<i class= "fas fa-signature"></i>`,
     category: `<p id="category">WILL's RELATIONSHIPS</p>`,
     description: `<p id="description">Lorem1 ipsum dolor sit amet.</p>`,
   },
@@ -31,30 +26,24 @@ const categoryArray = [
 let cardHtml = ""
 categoryArray.forEach((category) => {
     //console.log('category: ', category);
-cardHtml = `<!-- each card will flip all will have the same size but different colors -->
- <section id="category-card-grid" class="flip-card">
-  <section class="flip-card-inner">
-    <section id="icon" class="flip-card-front">
-      ${category.icon}
-    </section>
-    <section class="flip-card-back">
-    <h3>${category.category}</h3>
-      <p>${category.description}</p>
-    </section>
-  </section>
-</section> 
-<!-- CardGridEnd -->
+cardHtml = `<h3 id="category" class="category">${category.category}</h3>
+<p class="hide">${category.description}</p>
+<h3 id="category" class="category">${category.category}</h3>
+<p class="hide">${category.description}</p>
+<h3 id="category" class="category">${category.category}</h3>
+<p class="hide">${category.description}</p>
+<h3 id="category" class="category">${category.category}</h3>
+<p class="hide">${category.description}</p>
+<h3 id="category" class="category">${category.category}</h3>
+<p class="hide">${category.description}</p>
 <!-- Start the Game Button -->
-<!-- grid 1 row stretch all 5 columns -->
-<button id="startBtn">START</button>`
+<button id="startBtn" class="btn">START</button>`
 //console.log(cardHtml)
 })
 const cardContent = document.querySelector("#content")
 cardContent.innerHTML = cardHtml
 cardContent.style.color = "black"
 console.log(cardContent)
-const icon = document.querySelector("#icon")
-console.log('icon: ', icon);
 const startBtn = document.querySelector("#startBtn")
 console.log("startBtn: ", startBtn)
 //click event for start button
@@ -63,16 +52,10 @@ startBtn.addEventListener("click", () => {
   //display quiz.html goes here
 })
 
-//insertIcon()
 // insertCategory()
 // insertDescription
 //functions to return multipl icons, titles, and descriptions
-/* function insertIcon(){
-    iconArray.filter((icon) =>{
-         //console.log(icon)
-    return `${icon}`
-    })
-}
+
 function insertCategory(){
     categoryArray.forEach((category) =>{
         //console.log('category: ', category);
